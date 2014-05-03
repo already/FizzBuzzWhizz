6506a16badcaabb97e60c06d80f9fb1fc1f76d38
zhouqin <zhouqin.v@gmail.com>
2014-05-03 18:00:17 +0800

    Update FizzBuzzWhizz.java

--- FizzBuzzWhizz.java
+++ FizzBuzzWhizz.java
@@ -10,7 +10,11 @@ public class FizzBuzzWhizz {
 			throw new IllegalArgumentException("argument is illegal.");}
 		else{
 			for(int i=1;i<=100;i++){
-				if(!(i/100==a[0]&&print(s[0]) || i/10==a[0]&&print(s[0]) || i%10 ==a[0]&&print(s[0]) || a[0]*a[1]*a[2]==i&&print(s[0])&&print(s[1])&&print(s[2]) || a[0]*a[1]==i&&print(s[0])&&print(s[1]) || a[0]*a[2]==i&&print(s[0])&&print(s[2]) || a[1]*a[2]==i&&print(s[1])&&print(s[2]) || i%a[0]==0&&print(s[0]) || i%a[1]==0&&print(s[1]) || i%a[2]==0&&print(s[2]))){
+				if(!(i/100==a[0]&&print(s[0]) || i/10==a[0]&&print(s[0]) || i%10 ==a[0]&&print(s[0]) 
+				|| a[0]*a[1]*a[2]==i&&print(s[0])&&print(s[1])&&print(s[2]) || 
+				a[0]*a[1]==i&&print(s[0])&&print(s[1]) || a[0]*a[2]==i&&print(s[0])&&print(s[2]) ||
+				a[1]*a[2]==i&&print(s[1])&&print(s[2]) || i%a[0]==0&&print(s[0]) || i%a[1]==0&&print(s[1])
+				|| i%a[2]==0&&print(s[2]))){
 					System.out.print(i);
 				}
 				System.out.println();
